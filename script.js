0dfca83737c1bace30f4acf82e7dbfd8e4a6485b
Amina <87755052+mimalson@users.noreply.github.com>
2023-09-08 12:52:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const dayOfWeek = new Date().toLocaleDateString(undefined, { weekday: 'long' });
-    const currentTime = new Date().toUTCString();
+    const currentTime = new Date()
 
     // Update the current day and time elements
     document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = `Current Day: ${dayOfWeek}`;
-    document.querySelector("[data-testid='currentUTCTime']").textContent = `Current UTC Time: ${currentTime}`;
+    document.querySelector("[data-testid='currentUTCTime']")
 });
