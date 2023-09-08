55853c1f7cbdb8567ded96b607fea4dcf82fd325
Amina <87755052+mimalson@users.noreply.github.com>
2023-09-08 19:02:41 +0100

    Create scriptt.js

---
+++ scriptt.js
@@ -0,0 +1,19 @@
+function updateCurrentTimeAndDay() {
+            const currentTimeElement = document.getElementById('currentUTCTime');
+            const currentDayElement = document.getElementById('currentDayOfTheWeek');
+            const currentTimeMillis = new Date().getTime();
+            
+            // Get the current day of the week
+            const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            const currentDayIndex = new Date().getDay();
+            const currentDay = daysOfWeek[currentDayIndex];
+
+            currentTimeElement.textContent = `Current Time: ${currentTimeMillis} milliseconds`;
+            currentDayElement.textContent = `Current Day: ${currentDay}`;
+        }
+
+        // Update the time and day every second (1000 milliseconds)
+        setInterval(updateCurrentTimeAndDay, 1000);
+
+        // Initial update
+        updateCurrentTimeAndDay();
