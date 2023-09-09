147979149f1b94e6c05c8c51d46ff6075f1a13ba
Amina <87755052+mimalson@users.noreply.github.com>
2023-09-09 11:05:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const dayOfWeek = new Date().toLocaleDateString(undefined, { weekday: 'long' });
+    const currentTime = new Date().toUTCString();
 
-function updateClock() {
-            // Get the current time in milliseconds
-            const currentTimeMillis = Date.now();
-            document.querySelector('[data-testid="currentUTCTime"]').textContent = `Time: ${currentTimeMillis}`;
-
-            // Get the current day
-            const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-            const currentDate = new Date();
-            const currentDay = days[currentDate.getDay()];
-            document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Day: ${currentDay}`;
-        }
-
-        // Update the clock every second
-        setInterval(updateClock, 1000);
-
-        // Initial call to set the clock
-        updateClock();
-
-
-
-      //  const names = document.querySelector('[data-testid= "SlackUserName"]');
-        //names.innerHTML = 'meena__xx'
-
-      //  const track = document.querySelector('[data-testid= "myTrack"]');
-      //  track.innerHTML = 'Frontend'
-
-    //     const  githubURL= document.querySelector('[data-testid= "myTrack"]');
-    //  githubURL.innerHTML = 'Github Repository'
+    // Update the current day and time elements
+    document.querySelector("[data-testid='currentDayOfTheWeek']").textContent = `Current Day: ${dayOfWeek}`;
+    document.querySelector("[data-testid='currentUTCTime']").textContent = `Current UTC Time: ${currentTime}`;
+});
